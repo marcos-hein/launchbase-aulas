5b8b1cba038a029a3d43f8b14b5aa370d4a8337f
Vinicius Hein <vinicius.hein@gmail.com>
2020-07-13 21:17:57 -0300

    page about updated

--- module03/server.js
+++ module03/server.js
@@ -9,11 +9,24 @@ server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function(req, res) {
-    return res.render("about")
+    const about = {
+        avatar_url: "https://avatars1.githubusercontent.com/u/60493029?s=400&u=9aa39eaf9b1a95b7a0b44be821e2bfe920296841&v=4",
+        name: "Vinicius Hein",
+        role: "Desenvolvedor Web Full-Stack",
+        description: 'Estudante da <a href="https://rocketseat.com.br" target="_blank">Rocketseat</a>',
+        links: [
+            { name: "Github", url: "https://github.com/vinicius-hein" },
+            { name: "LinkedIn", url: "https://linkedin.com/in/vinicius-hein" }
+        ]
+
+    }
+
+    return res.render("about", { about })
 })
 
 server.get("/portifolio", function(req, res) {
