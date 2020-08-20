00957092d71269d5085d40d7ace9142056ef687d
Vinicius Hein <vinicius.hein@gmail.com>
2020-08-19 21:04:12 -0300

    validating data on the back end

--- module04/routes.js
+++ module04/routes.js
@@ -14,7 +14,17 @@ routes.get('/create', function(req, res) {
 })
 
 routes.post('/instructors', function(req, res) {
+
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send('Please, fill all fields')
+        }
+    }
+
     return res.send(req.body)
+
 })
 
 routes.get('/members', function(req, res) {
