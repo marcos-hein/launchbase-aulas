232c753e08cfd9ec127609ac609c75796eb1e4e9
Vinicius Hein <vinicius.hein@gmail.com>
2020-08-25 20:59:44 -0300

    finalizing the instructor's presentation page

--- module04/instructors.js
+++ module04/instructors.js
@@ -17,7 +17,7 @@ exports.show = function(req, res) {
         ...foundInstructor,
         age: age(foundInstructor.birth),
         services: foundInstructor.services.split(","),
-        created_at: "",
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundInstructor.created_at)
     }
 
     return res.render("./instructors/show", { instructor })
