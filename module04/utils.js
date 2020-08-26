51e49a50b65cd21da6c3e51d104dd6031a10d597
Vinicius Hein <vinicius.hein@gmail.com>
2020-08-25 22:24:28 -0300

    formatting dates for the form

--- module04/utils.js
+++ module04/utils.js
@@ -14,5 +14,16 @@ module.exports = {
         }
     
         return age
+    },
+    date: function(timestamp) {
+        const date = new Date(timestamp)
+
+        const year = date.getUTCFullYear()
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2)
+        const day = `0${date.getUTCDate()}`.slice(-2)
+
+        console.log(`${year}-${month}-${day}`)
+
+        return `${year}-${month}-${day}`
     }
 }
