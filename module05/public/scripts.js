9e7d670a89d69d11f02d7f4f10ac43a9a84c764e
Vinicius Hein <vinicius.hein@gmail.com>
2020-12-01 20:12:35 -0300

    pagination algorithm on the front-end

--- module05/public/scripts.js
+++ module05/public/scripts.js
@@ -6,3 +6,37 @@ for (const item of menuItems) {
         item.classList.add("active")
     }
 }
+
+// Paginação
+// totalPages = 20
+// selectedPage = 15
+// [1,..., 13, 14, 15, 16, 17, ..., 20]
+
+let totalPages = 20,
+    selectedPage = 16,
+    pages = [],
+    oldPage
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
+ 
+    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+
+        if (oldPage && currentPage - oldPage > 2) {
+            pages.push("...")
+        }
+
+        if (currentPage - oldPage == 2) {
+            pages.push(oldPage + 1)
+        }
+
+        pages.push(currentPage)
+
+        oldPage = currentPage
+    }
+}
+
+console.log(pages)
